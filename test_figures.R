965a450e6f4ad285ef4d674bcb101cb28ae809f5
Luciana-cloud <lucianagoku@hotmail.com>
2023-10-13 02:10:27 -0700

    - Manova test + assumptions - VIF (multicollinearity) - Partial Least Squares Regression (PLSR) # Discuss pipeline and potential results

--- test_figures.R
+++ test_figures.R
@@ -243,24 +243,6 @@ ggboxplot(my_data, x = "Site", y = "Respiration",
           fill = "AI") + stat_compare_means() + 
   scale_fill_gradient(low="blue", high="red")
 
-# Linear model between soil aridity and physicochemical variables ####
-
-model_1 = lm(AI~Water_content,data=my_data)
-model_2 = lm(AI~Water_content+Soil_Temp,data=my_data) 
-model_3 = lm(AI~Water_content+Soil_Temp+SOM_perc,data=my_data)
-model_4 = lm(AI~Water_content+Soil_Temp+SOM_perc+pH,data=my_data)
-model_5 = lm(AI~Water_content+Soil_Temp+SOM_perc+pH+Litter,data=my_data)
-model_6 = lm(AI~Water_content+Soil_Temp+SOM_perc+pH+Litter+PO43,data=my_data)
-model_7 = lm(AI~Water_content+SOM_perc+pH+PO43,data=my_data)
-
-anova(model_1,model_2,model_3,model_4,model_5,model_6,model_7)
-AIC(model_1,model_2,model_3,model_4,model_5,model_6,model_7)
-
-shapiro.test(model_7$residuals)
-plot(model_7, which = 1)
-plot(model_7, which = 2)
-summary(model_7)
-
 # PCA ####
 
 results          = prcomp(my_data[,c(8,9,10,11,13,14,16,20,21,22,23,24,25,26,30,31,32)],
@@ -274,13 +256,138 @@ results$x
 
 results$sdev^2 / sum(results$sdev^2)
 
-# Manova test
+# Manova ####
 
-y_variables = as.data.frame((my_data[,c(35,42:53,56:64)]))
-corre       = as.data.frame(cor(y_variables))
-mod         = manova(as.matrix(y_variables) ~ as.factor(my_data$AI))
+# Manova Asumptions ####
+
+y_variables = as.data.frame((my_data[,c(2,35,42:53,56:64)]))
+
+library(rstatix)
+library(broom)
+
+# Outliers ####
+out = as.data.frame(y_variables %>% group_by(Site) %>% identify_outliers("CO2_dark"))
+# There are outliers but we are keeping them for the analysis
+
+# Multivariate outliers ####
+y_variables_2       = scale(y_variables[,c(2:6,8,9,11:13,15:23)],center = FALSE)
+mahal               = mahalanobis(y_variables_2, colMeans(y_variables_2), cov(y_variables_2))
+p_val               = pchisq(mahal, df=3, lower.tail=FALSE)
+y_variables_2       = as.data.frame(cbind(y_variables_2,mahal,p_val))
+# There are outliers but we are keeping them for the analysis
+
+# Normality ####
+y_variables_2      = as.data.frame(cbind(y_variables$Site,y_variables_2))
+ggqqplot(y_variables_2, "CO2_dark", facet.by = "y_variables$Site",
+         ylab = "CO2_dark", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "chla", facet.by = "y_variables$Site",
+         ylab = "chla", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "chlb", facet.by = "y_variables$Site",
+         ylab = "chlb", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "carotene", facet.by = "y_variables$Site",
+         ylab = "carotene", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "EPS", facet.by = "y_variables$Site",
+         ylab = "EPS", ggtheme = theme_bw())
+ggqqplot(y_variables, "alpha", facet.by = "Site",
+         ylab = "alpha", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "beta", facet.by = "y_variables$Site",
+         ylab = "beta", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "xyl", facet.by = "y_variables$Site",
+         ylab = "xyl", ggtheme = theme_bw())
+ggqqplot(y_variables, "cbh", facet.by = "Site",
+         ylab = "cbh", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "gla", facet.by = "y_variables$Site",
+         ylab = "gla", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "fos", facet.by = "y_variables$Site",
+         ylab = "fos", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "leu", facet.by = "y_variables$Site",
+         ylab = "leu", ggtheme = theme_bw())
+ggqqplot(y_variables, "phe", facet.by = "Site",
+         ylab = "phe", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "mcrA", facet.by = "y_variables$Site",
+         ylab = "mcrA", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "pmoA", facet.by = "y_variables$Site",
+         ylab = "pmoA", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "nifH", facet.by = "y_variables$Site",
+         ylab = "nifH", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "AOA", facet.by = "y_variables$Site",
+         ylab = "AOA", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "AOB", facet.by = "y_variables$Site",
+         ylab = "AOB", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "qnorB", facet.by = "y_variables$Site",
+         ylab = "qnorB", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "nosZ", facet.by = "y_variables$Site",
+         ylab = "nosZ", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "phoD", facet.by = "y_variables$Site",
+         ylab = "phoD", ggtheme = theme_bw())
+ggqqplot(y_variables_2, "Respiration", facet.by = "y_variables$Site",
+         ylab = "Respiration", ggtheme = theme_bw())
+# I think most of the variables are normally distributed and MANOVA is robust with
+# slightly violations of normality. 
+
+# Multivariate normality ####
+mshapiro_test((y_variables_2[,2:22]))
+# Not really normal
+
+# Multicollinearity ####
+cor.mat <- y_variables[,2:23] %>% cor_mat()
+cor.mat %>% cor_reorder() %>% pull_lower_triangle() %>% cor_plot(label = TRUE)
+# New selected variables
+y_variables_3 = as.data.frame((y_variables[,c(1:3,5:20,22,23)]))
+
+# Linearity assumption ####
+library(GGally)
+linear_ass <- y_variables_3 %>% group_by(Site) %>% doo(~ggpairs(.) + theme_bw(), result = "plots")
+linear_ass$plots
+
+# Homogeneity of covariances ####
+# Balance design and we could continue with the analysis. We will use the 
+# Pillai’s multivariate statistic as a more robust metric
+
+# Homogeneity of variance assumption ####
+a = as.data.frame(colnames(y_variables_3))
+y_variables_3 %>% gather(key = "variable", value = "value", a[2:21,]) %>%
+  group_by(variable) %>% levene_test(sqrt(value) ~ as.factor(Site))
+# Squaring the variables solves almost completely the homogeneity of variances
+
+# Manova test ####
+mod         = manova(as.matrix(y_variables_3[,2:21]) ~ (my_data$AI))
 summary(mod,summary=TRUE)
-summary.aov(mod)
 
-mod         = manova(as.matrix(y_variables) ~ as.factor(my_data$AI),
-                     subset(as.factor(my_data$AI) %in% c(1,2)))
+# Univariate one-way ANOVA ####
+grouped.data <- y_variables_3 %>% gather(key = "variable", value = "value", a[2:21,]) %>%
+  group_by(variable)
+grouped.data %>% kruskal_test(value ~ my_data$AI)
+
+# What variables to use in the MANOVA as covariables
+x_variables = as.data.frame((my_data[,c(2,5:8,10,11,13:16,20:26,30:32)]))
+
+cor.mat <- x_variables[,2:21] %>% cor_mat()
+cor.mat %>% cor_reorder() %>% pull_lower_triangle() %>% cor_plot(label = TRUE)
+# New selected variables
+x_variables_1 = as.data.frame((x_variables[,c(1,5:7,10,12:15,19:21)]))
+
+# VIF ####
+library(car)
+cor.mat <- x_variables_1[,2:12] %>% cor_mat()
+model <- lm(cbind(y_variables_3$CO2_dark+y_variables_3$chla+y_variables_3$carotene+
+                    y_variables_3$EPS) 
+            ~ x_variables_1$Soil_Temp+
+              x_variables_1$Water_content+x_variables_1$SOM_perc+x_variables_1$TC_perc+
+              x_variables_1$C_N+x_variables_1$NH4+x_variables_1$PO43+x_variables_1$SO42+
+              x_variables_1$Litter+x_variables_1$L_TC_perc+x_variables_1$L_TN_perc+as.numeric(my_data$AI))
+car::vif(model)
+
+library(pls)
+model <- plsr(y_variables_3$CO2_dark~x_variables_1$Soil_Temp+
+                x_variables_1$Water_content+x_variables_1$SOM_perc+x_variables_1$TC_perc+
+                x_variables_1$C_N+x_variables_1$NH4+x_variables_1$PO43+x_variables_1$SO42+
+                x_variables_1$Litter+x_variables_1$L_TC_perc+x_variables_1$L_TN_perc, scale=TRUE, validation="CV")
+summary(model)
+coefficients = coef(model)
+
+cv = RMSEP(model)
+best.dims = which.min(cv$val[estimate = "adjCV", , ]) - 1
+R2(model)
+
+# Reference paper: https://bsssjournals.onlinelibrary.wiley.com/doi/epdf/10.1111/ejss.13419
