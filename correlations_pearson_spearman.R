15141dad6c3eec88db58cc7a0b235004f550d1df
AnnaDomePas <xeanna8@gmail.com>
2024-09-30 16:07:49 +0200

    RDA analyses

--- correlations_pearson_spearman.R
+++ correlations_pearson_spearman.R
@@ -10,6 +10,8 @@ library("gridExtra")
 my_data <- read_excel("SP_metadata_2021.xlsx")
 mit_16S <- read_excel("C:/Users/Anna/OneDrive - Universitat de Girona/GRADCATCH/ANALISIS/amplicon_sequencing_2021_JD/16S/Alphadiv_SP_16S_mit.xlsx")
 mit_18S <- read_excel("C:/Users/Anna/OneDrive - Universitat de Girona/GRADCATCH/ANALISIS/amplicon_sequencing_2021_JD/ITS2/Alphadiv_SP_ITS2_mit.xlsx")
+mit_16S <- read_excel("C:/Users/ecologia.PCECO002/OneDrive - Universitat de Girona/GRADCATCH/ANALISIS/amplicon_sequencing_2021_JD/16S/Alphadiv_SP_16S_mit.xlsx")
+mit_18S <- read_excel("C:/Users/ecologia.PCECO002/OneDrive - Universitat de Girona/GRADCATCH/ANALISIS/amplicon_sequencing_2021_JD/ITS2/Alphadiv_SP_ITS2_mit.xlsx")
 
 mit_16S <- rename(mit_16S,
                   Site = GxS,
@@ -33,13 +35,17 @@ mit_18S <- merge(aridity, mit_18S, by = "Site")
 mit_16S <- mit_16S[,-c(6:11)]
 mit_18S <- mit_18S[,-c(6:11)]
 
+mit_16S$Aridity <- (1 - mit_16S$AI)
+mit_18S$Aridity <- (1 - mit_18S$AI)
+
+
 rm(aridity)
 
 
 #PHYSICOCHEMICAL ----
 
 #Clean non-interesting variables:
-cor_data <- my_data[,c(2,7,10,13,20:23,33,34,28,76,30,86,92)]
+cor_data <- my_data[-c(61:63),c(2,7,10,13,20:23,33,34,28,76,30,86,92)]
 
 replace_na_with_mean <- function(data) {
   data %>%
@@ -54,26 +60,11 @@ cor_data <- cor_data %>%
   summarise_all(mean)
 
 
-#PEARSON:
-numerical_variables <- sapply(cor_data, is.numeric)
-numerical_variables_names <- names(numerical_variables)[numerical_variables]
 
-all_plots <- list()
+##### With Aridity
 
-for (var in numerical_variables_names) {
-  if (var != "AI") {
-    plot_title <- paste("AI vs", var)
-    all_plots[[length(all_plots) + 1]] <- ggscatter(cor_data, x = "AI", y = var,
-                                                    add = "reg.line", conf.int = TRUE,
-                                                    cor.coef = TRUE, cor.method = "pearson",
-                                                    xlab = "AI", ylab = var,
-                                                    main = plot_title)
-  }
-}
-
-grid.arrange(grobs = all_plots, nrow = 2)
-
-
+cor_data$Aridity <- (1 - cor_data$AI)
+cor_data <- cor_data[,-2]
 
 
 #SPEARMAN:
@@ -83,52 +74,11 @@ numerical_variables_names <- names(numerical_variables)[numerical_variables]
 all_plots <- list()
 
 for (var in numerical_variables_names) {
-  if (var != "AI") {
-    plot_title <- paste("AI vs", var)
-    all_plots[[length(all_plots) + 1]] <- ggscatter(cor_data, x = "AI", y = var,
+  if (var != "Aridity") {
+    plot_title <- paste("Aridity vs", var)
+    all_plots[[length(all_plots) + 1]] <- ggscatter(cor_data, x = "Aridity", y = var,
                                                     add = "reg.line", conf.int = TRUE,
                                                     cor.coef = TRUE, cor.method = "spearman",
-                                                    xlab = "AI", ylab = var,
-                                                    main = plot_title)
-  }
-}
-
-grid.arrange(grobs = all_plots, nrow = 2)
-
-
-
-# FUNCTIONS ----
-#Clean non-interesting variables:
-cor_data <- my_data[,c(2,7,40,41,46:53,64)]
-
-replace_na_with_mean <- function(data) {
-  data %>%
-    group_by(Site) %>%
-    mutate_all(~ ifelse(is.na(.), mean(., na.rm = TRUE), .)) %>%
-    ungroup()
-}
-cor_data <- replace_na_with_mean(cor_data)
-
-cor_data <- cor_data %>%
-  group_by(Site) %>%
-  summarise_all(mean)
-
-cor_data$aridity <- (1-cor_data$AI)
-cor_data <- cor_data[,-c(2)]
-
-
-#PEARSON:
-numerical_variables <- sapply(cor_data, is.numeric)
-numerical_variables_names <- names(numerical_variables)[numerical_variables]
-
-all_plots <- list()
-
-for (var in numerical_variables_names) {
-  if (var != "aridity") {
-    plot_title <- paste("Aridity vs", var)
-    all_plots[[length(all_plots) + 1]] <- ggscatter(cor_data, x = "aridity", y = var,
-                                                    add = "reg.line", conf.int = TRUE,
-                                                    cor.coef = TRUE, cor.method = "pearson",
                                                     xlab = "Aridity", ylab = var,
                                                     main = plot_title)
   }
@@ -137,6 +87,99 @@ for (var in numerical_variables_names) {
 grid.arrange(grobs = all_plots, nrow = 2)
 
 
+
+
+# #### With AI
+# 
+# #PEARSON:
+# numerical_variables <- sapply(cor_data, is.numeric)
+# numerical_variables_names <- names(numerical_variables)[numerical_variables]
+# 
+# all_plots <- list()
+# 
+# for (var in numerical_variables_names) {
+#   if (var != "AI") {
+#     plot_title <- paste("AI vs", var)
+#     all_plots[[length(all_plots) + 1]] <- ggscatter(cor_data, x = "AI", y = var,
+#                                                     add = "reg.line", conf.int = TRUE,
+#                                                     cor.coef = TRUE, cor.method = "pearson",
+#                                                     xlab = "AI", ylab = var,
+#                                                     main = plot_title)
+#   }
+# }
+# 
+# grid.arrange(grobs = all_plots, nrow = 2)
+# 
+# 
+# 
+# 
+# #SPEARMAN:
+# numerical_variables <- sapply(cor_data, is.numeric)
+# numerical_variables_names <- names(numerical_variables)[numerical_variables]
+# 
+# all_plots <- list()
+# 
+# for (var in numerical_variables_names) {
+#   if (var != "AI") {
+#     plot_title <- paste("AI vs", var)
+#     all_plots[[length(all_plots) + 1]] <- ggscatter(cor_data, x = "AI", y = var,
+#                                                     add = "reg.line", conf.int = TRUE,
+#                                                     cor.coef = TRUE, cor.method = "spearman",
+#                                                     xlab = "AI", ylab = var,
+#                                                     main = plot_title)
+#   }
+# }
+# 
+# grid.arrange(grobs = all_plots, nrow = 2)
+# 
+# 
+
+# FUNCTIONS ----
+#Clean non-interesting variables:
+cor_data <- my_data[-c(61:63),c(2,7,40,41,46:53,64)]
+
+replace_na_with_mean <- function(data) {
+  data %>%
+    group_by(Site) %>%
+    mutate_all(~ ifelse(is.na(.), mean(., na.rm = TRUE), .)) %>%
+    ungroup()
+}
+cor_data <- replace_na_with_mean(cor_data)
+
+cor_data <- cor_data %>%
+  group_by(Site) %>%
+  summarise_all(mean)
+
+cor_data$Aridity <- (1-cor_data$AI)
+cor_data <- cor_data[,-c(2)]
+
+
+
+
+
+
+
+# #PEARSON:
+# numerical_variables <- sapply(cor_data, is.numeric)
+# numerical_variables_names <- names(numerical_variables)[numerical_variables]
+# 
+# all_plots <- list()
+# 
+# for (var in numerical_variables_names) {
+#   if (var != "aridity") {
+#     plot_title <- paste("Aridity vs", var)
+#     all_plots[[length(all_plots) + 1]] <- ggscatter(cor_data, x = "aridity", y = var,
+#                                                     add = "reg.line", conf.int = TRUE,
+#                                                     cor.coef = TRUE, cor.method = "pearson",
+#                                                     xlab = "Aridity", ylab = var,
+#                                                     main = plot_title)
+#   }
+# }
+# 
+# grid.arrange(grobs = all_plots, nrow = 2)
+# 
+
+
 #SPEARMAN:
 numerical_variables <- sapply(cor_data, is.numeric)
 numerical_variables_names <- names(numerical_variables)[numerical_variables]
@@ -144,12 +187,12 @@ numerical_variables_names <- names(numerical_variables)[numerical_variables]
 all_plots <- list()
 
 for (var in numerical_variables_names) {
-  if (var != "AI") {
-    plot_title <- paste("AI vs", var)
-    all_plots[[length(all_plots) + 1]] <- ggscatter(cor_data, x = "AI", y = var,
+  if (var != "Aridity") {
+    plot_title <- paste("Aridity vs", var)
+    all_plots[[length(all_plots) + 1]] <- ggscatter(cor_data, x = "Aridity", y = var,
                                                     add = "reg.line", conf.int = TRUE,
                                                     cor.coef = TRUE, cor.method = "spearman",
-                                                    xlab = "AI", ylab = var,
+                                                    xlab = "Aridity", ylab = var,
                                                     main = plot_title)
   }
 }
@@ -161,24 +204,24 @@ grid.arrange(grobs = all_plots, nrow = 2)
 # WITHOUT SP06:
 cor_data <- subset(cor_data, Site != "SP06")
 
-# PEARSON:
-numerical_variables <- sapply(cor_data, is.numeric)
-numerical_variables_names <- names(numerical_variables)[numerical_variables]
-
-all_plots <- list()
-
-for (var in numerical_variables_names) {
-  if (var != "AI") {
-    plot_title <- paste("AI vs", var)
-    all_plots[[length(all_plots) + 1]] <- ggscatter(cor_data, x = "AI", y = var,
-                                                    add = "reg.line", conf.int = TRUE,
-                                                    cor.coef = TRUE, cor.method = "pearson",
-                                                    xlab = "AI", ylab = var,
-                                                    main = plot_title)
-  }
-}
-
-grid.arrange(grobs = all_plots, nrow = 2)
+# # PEARSON:
+# numerical_variables <- sapply(cor_data, is.numeric)
+# numerical_variables_names <- names(numerical_variables)[numerical_variables]
+# 
+# all_plots <- list()
+# 
+# for (var in numerical_variables_names) {
+#   if (var != "Aridity") {
+#     plot_title <- paste("Aridity vs", var)
+#     all_plots[[length(all_plots) + 1]] <- ggscatter(cor_data, x = "Aridity", y = var,
+#                                                     add = "reg.line", conf.int = TRUE,
+#                                                     cor.coef = TRUE, cor.method = "pearson",
+#                                                     xlab = "Aridity", ylab = var,
+#                                                     main = plot_title)
+#   }
+# }
+# 
+# grid.arrange(grobs = all_plots, nrow = 2)
 
 
 
@@ -189,12 +232,12 @@ numerical_variables_names <- names(numerical_variables)[numerical_variables]
 all_plots <- list()
 
 for (var in numerical_variables_names) {
-  if (var != "AI") {
-    plot_title <- paste("AI vs", var)
-    all_plots[[length(all_plots) + 1]] <- ggscatter(cor_data, x = "AI", y = var,
+  if (var != "Aridity") {
+    plot_title <- paste("Aridity vs", var)
+    all_plots[[length(all_plots) + 1]] <- ggscatter(cor_data, x = "Aridity", y = var,
                                                     add = "reg.line", conf.int = TRUE,
                                                     cor.coef = TRUE, cor.method = "spearman",
-                                                    xlab = "AI", ylab = var,
+                                                    xlab = "Aridity", ylab = var,
                                                     main = plot_title)
   }
 }
@@ -211,24 +254,24 @@ grid.arrange(grobs = all_plots, nrow = 2)
 
 ## 16S ----
 
-#PEARSON:
-numerical_variables <- sapply(mit_16S, is.numeric)
-numerical_variables_names <- names(numerical_variables)[numerical_variables]
-
-all_plots <- list()
-
-for (var in numerical_variables_names) {
-  if (var != "AI") {
-    plot_title <- paste("AI vs", var)
-    all_plots[[length(all_plots) + 1]] <- ggscatter(mit_16S, x = "AI", y = var,
-                                                    add = "reg.line", conf.int = TRUE,
-                                                    cor.coef = TRUE, cor.method = "pearson",
-                                                    xlab = "AI", ylab = var,
-                                                    main = plot_title)
-  }
-}
-
-grid.arrange(grobs = all_plots, nrow = 1)
+# #PEARSON:
+# numerical_variables <- sapply(mit_16S, is.numeric)
+# numerical_variables_names <- names(numerical_variables)[numerical_variables]
+# 
+# all_plots <- list()
+# 
+# for (var in numerical_variables_names) {
+#   if (var != "AI") {
+#     plot_title <- paste("AI vs", var)
+#     all_plots[[length(all_plots) + 1]] <- ggscatter(mit_16S, x = "AI", y = var,
+#                                                     add = "reg.line", conf.int = TRUE,
+#                                                     cor.coef = TRUE, cor.method = "pearson",
+#                                                     xlab = "AI", ylab = var,
+#                                                     main = plot_title)
+#   }
+# }
+# 
+# grid.arrange(grobs = all_plots, nrow = 1)
 
 
 #SPEARMAN:
@@ -238,12 +281,12 @@ numerical_variables_names <- names(numerical_variables)[numerical_variables]
 all_plots <- list()
 
 for (var in numerical_variables_names) {
-  if (var != "AI") {
-    plot_title <- paste("AI vs", var)
-    all_plots[[length(all_plots) + 1]] <- ggscatter(mit_16S, x = "AI", y = var,
+  if (var != "Aridity") {
+    plot_title <- paste("Aridity vs", var)
+    all_plots[[length(all_plots) + 1]] <- ggscatter(mit_16S, x = "Aridity", y = var,
                                                     add = "reg.line", conf.int = TRUE,
                                                     cor.coef = TRUE, cor.method = "spearman",
-                                                    xlab = "AI", ylab = var,
+                                                    xlab = "Aridity", ylab = var,
                                                     main = plot_title)
   }
 }
@@ -253,23 +296,23 @@ grid.arrange(grobs = all_plots, nrow = 1)
 
 
 ## 18S ----
-numerical_variables <- sapply(mit_18S, is.numeric)
-numerical_variables_names <- names(numerical_variables)[numerical_variables]
+# numerical_variables <- sapply(mit_18S, is.numeric)
+# numerical_variables_names <- names(numerical_variables)[numerical_variables]
+# 
+# all_plots <- list()
 
-all_plots <- list()
-
-for (var in numerical_variables_names) {
-  if (var != "AI") {
-    plot_title <- paste("AI vs", var)
-    all_plots[[length(all_plots) + 1]] <- ggscatter(mit_18S, x = "AI", y = var,
-                                                    add = "reg.line", conf.int = TRUE,
-                                                    cor.coef = TRUE, cor.method = "pearson",
-                                                    xlab = "AI", ylab = var,
-                                                    main = plot_title)
-  }
-}
-
-grid.arrange(grobs = all_plots, nrow = 1)
+# for (var in numerical_variables_names) {
+#   if (var != "AI") {
+#     plot_title <- paste("AI vs", var)
+#     all_plots[[length(all_plots) + 1]] <- ggscatter(mit_18S, x = "AI", y = var,
+#                                                     add = "reg.line", conf.int = TRUE,
+#                                                     cor.coef = TRUE, cor.method = "pearson",
+#                                                     xlab = "AI", ylab = var,
+#                                                     main = plot_title)
+#   }
+# }
+# 
+# grid.arrange(grobs = all_plots, nrow = 1)
 
 
 #SPEARMAN:
@@ -279,12 +322,12 @@ numerical_variables_names <- names(numerical_variables)[numerical_variables]
 all_plots <- list()
 
 for (var in numerical_variables_names) {
-  if (var != "AI") {
-    plot_title <- paste("AI vs", var)
-    all_plots[[length(all_plots) + 1]] <- ggscatter(mit_18S, x = "AI", y = var,
+  if (var != "Aridity") {
+    plot_title <- paste("Aridity vs", var)
+    all_plots[[length(all_plots) + 1]] <- ggscatter(mit_18S, x = "Aridity", y = var,
                                                     add = "reg.line", conf.int = TRUE,
                                                     cor.coef = TRUE, cor.method = "spearman",
-                                                    xlab = "AI", ylab = var,
+                                                    xlab = "Aridity", ylab = var,
                                                     main = plot_title)
   }
 }
